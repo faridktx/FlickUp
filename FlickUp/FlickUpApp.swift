5d3f71567b87c4b30a109c1708cfcafde5b6ee3c
faridktx <abbasovfarid22@gmail.com>
2025-04-19 15:24:59 -0500

    Initial Commit

---
+++ FlickUp/FlickUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickUpApp.swift
+//  FlickUp
+//
+//  Created by Farid Abbasov on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FlickUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
